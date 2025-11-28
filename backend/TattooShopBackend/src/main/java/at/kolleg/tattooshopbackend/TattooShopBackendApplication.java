0ba83d4117d2f52d700112de1ad4b85789836869
leamarieb <167232705+leamarieb@users.noreply.github.com>
2025-11-28 15:03:48 +0100

    Spring Boot Projekt generiert

---
+++ backend/TattooShopBackend/src/main/java/at/kolleg/tattooshopbackend/TattooShopBackendApplication.java
@@ -0,0 +1,13 @@
+package at.kolleg.tattooshopbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TattooShopBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TattooShopBackendApplication.class, args);
+    }
+
+}
